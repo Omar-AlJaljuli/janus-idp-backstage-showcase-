6296a8dc85b8bb3fa70d6bd5cebe3af0905a1f5b
Omar Al Jaljuli <oaljalju@redhat.com>
2024-11-20 14:54:00 -0500

    debug mode

--- e2e-tests/playwright/e2e/instance-health-check.spec.ts
+++ e2e-tests/playwright/e2e/instance-health-check.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('Application health check', async ({ request }) => {
+  console.log(`Url instance: ${process.env.BASE_URL}/healthcheck`);
   const healthCheckEndpoint = `${process.env.BASE_URL}/healthcheck`;
 
   const response = await request.get(healthCheckEndpoint);
