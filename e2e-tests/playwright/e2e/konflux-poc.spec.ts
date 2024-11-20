85eb38376226c53d8f54c0fe1a24c1f4f2cbbed7
Omar Al Jaljuli <oaljalju@redhat.com>
2024-11-20 14:42:34 -0500

    remove setting context

--- e2e-tests/playwright/e2e/konflux-poc.spec.ts
+++ e2e-tests/playwright/e2e/konflux-poc.spec.ts
@@ -3,12 +3,8 @@ import { Common } from '../utils/Common';
 
 test.describe('Verify TLS configuration with external Postgres DB', () => {
   test('Verify successful DB connection and display of expected entities in the Catalog', async ({
-    browser,
+    page,
   }) => {
-    const context = await browser.newContext({
-      ignoreHTTPSErrors: true,
-    });
-    const page = await context.newPage();
     const common = new Common(page);
     await common.loginAsGuest();
   });
